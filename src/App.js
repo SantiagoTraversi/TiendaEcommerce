ee522f4d4db22367bed95d73a09d8250a6a88340
Santiago Traversi <santiago.traversi@hotmail.com>
2022-09-06 19:15:43 -0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Navbar from './componentes/Navbar.jsx';
-import ItemListContainer from './componentes/ItemListContainer.jsx'; 
+import ItemListContainer from './componentes/ItemListContainer.jsx';
 import Footer from './componentes/Footer.jsx';
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <h1>Ecommerce Tienda de Dulces  </h1>
       <ItemListContainer />
       <Footer />
     </div>
